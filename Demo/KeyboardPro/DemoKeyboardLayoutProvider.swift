a6695761d2c4a320ed651f68c849a32b5c6af893
pete-air <peter.chambers@airapps.com>
2023-06-13 12:16:18 +0200

    code comments

--- Demo/KeyboardPro/DemoKeyboardLayoutProvider.swift
+++ Demo/KeyboardPro/DemoKeyboardLayoutProvider.swift
@@ -1,44 +1,44 @@
+////
+////  DemoKeyboardLayoutProvider.swift
+////  KeyboardPro
+////
+////  Created by Daniel Saidi on 2022-12-21.
+////  Copyright © 2022-2023 Daniel Saidi. All rights reserved.
+////
 //
-//  DemoKeyboardLayoutProvider.swift
-//  KeyboardPro
+//import KeyboardKitPro
 //
-//  Created by Daniel Saidi on 2022-12-21.
-//  Copyright © 2022-2023 Daniel Saidi. All rights reserved.
+///**
+// This demo-specific appearance inherits the standard one and
+// adds a locale button next to space.
 //
-
-import KeyboardKitPro
-
-/**
- This demo-specific appearance inherits the standard one and
- adds a locale button next to space.
-
- ``KeyboardViewController`` registers this class to show you
- how you can set up a custom layout provider.
-
- The locale button is only be added if the keyboard has many
- locales. The KeyboardKit Pro demo automatically sets up the
- keyboard with all locales that are available in the license.
- */
-class DemoKeyboardLayoutProvider: StandardKeyboardLayoutProvider {
-
-    override func keyboardLayout(for context: KeyboardContext) -> KeyboardLayout {
-        let layout = super.keyboardLayout(for: context)
-        // layout.tryInsertDictationButton()
-        guard context.locales.count > 1 else { return layout }
-        layout.tryInsertLocaleSwitcher()
-        return layout
-    }
-}
-
-private extension KeyboardLayout {
-
-    func tryInsertDictationButton() {
-        guard let item = tryCreateBottomRowItem(for: .dictation) else { return }
-        itemRows.insert(item, after: .space, atRow: bottomRowIndex)
-    }
-
-    func tryInsertLocaleSwitcher() {
-        guard let item = tryCreateBottomRowItem(for: .nextLocale) else { return }
-        itemRows.insert(item, after: .space, atRow: bottomRowIndex)
-    }
-}
+// ``KeyboardViewController`` registers this class to show you
+// how you can set up a custom layout provider.
+//
+// The locale button is only be added if the keyboard has many
+// locales. The KeyboardKit Pro demo automatically sets up the
+// keyboard with all locales that are available in the license.
+// */
+//class DemoKeyboardLayoutProvider: StandardKeyboardLayoutProvider {
+//
+//    override func keyboardLayout(for context: KeyboardContext) -> KeyboardLayout {
+//        let layout = super.keyboardLayout(for: context)
+//        // layout.tryInsertDictationButton()
+//        guard context.locales.count > 1 else { return layout }
+//        layout.tryInsertLocaleSwitcher()
+//        return layout
+//    }
+//}
+//
+//private extension KeyboardLayout {
+//
+//    func tryInsertDictationButton() {
+//        guard let item = tryCreateBottomRowItem(for: .dictation) else { return }
+//        itemRows.insert(item, after: .space, atRow: bottomRowIndex)
+//    }
+//
+//    func tryInsertLocaleSwitcher() {
+//        guard let item = tryCreateBottomRowItem(for: .nextLocale) else { return }
+//        itemRows.insert(item, after: .space, atRow: bottomRowIndex)
+//    }
+//}
